8a0a3b9a3df33dda007496a3a21c4686b19fe247
Thanh Vu <thanh@putatu.com>
2019-06-01 21:03:23 +0700

    Make typescript hot loading

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,15 @@
 import * as React from "react";
+import { hot } from "react-hot-loader";
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface HelloProps {
+  compiler: string;
+  framework: string;
+}
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+const Hello = (props: HelloProps) => (
+  <h1>
+    Hello ok {props.compiler} and {props.framework}!
+  </h1>
+);
+
+export default hot(module)(Hello);
